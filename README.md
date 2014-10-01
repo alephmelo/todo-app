b424eee6dbf8c90de63b6b51b902e76df5268549
Aleph Melo <alephmelo@icloud.com>
2014-10-01 04:21:57 -0300

    Uploading TodoApp

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-TodoApp
+## TodoApp
+This is just a simple hybrid ionic/angular based app.
+You can try it out in your web browser, just clone this repo and open it 
+in a localhost server.
 =======
---
+++ todo.js
@@ -0,0 +1,75 @@
+angular.module('todo', ['ionic','todo.services'])
+
+.controller('TodoCtrl', function($scope, $ionicModal, $ionicPopup, SQLService) {
+  
+	SQLService.setup();
+		
+	$scope.loadTask = function() {
+		SQLService.all().then(function (results) {
+			$scope.tasks = results;
+		});	
+	}
+
+	$scope.loadTask(); 
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+  $scope.about = function() {
+   var alertPopup = $ionicPopup.alert({
+     title: 'Follow me on Twitter && Github.',
+     template: '@Alephbreno | Alephmelo',
+    });
+  };
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+	SQLService.set(task.title);
+	$scope.loadTask();
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+  
+  $scope.onItemDelete = function(taskid) {
+	$ionicPopup.confirm({
+	  title: 'Confirme Exclusão',
+	  content: 'Você tem certeza que quer excluir?',
+    cancelText: 'Cancelar',
+	}).then(function(res) {
+	  if(res) {
+		SQLService.del(taskid);
+		$scope.loadTask();
+	  } 
+	});
+  };
+  
+  $scope.onItemEdit = function(taskid) {
+    $ionicPopup.prompt({
+	  title: 'Atualizar Tarefa',
+	  subTitle: 'Digite nova tarefa',
+    cancelText: 'Cancelar',
+	}).then(function(res) {
+		SQLService.edit(res, taskid);
+		$scope.loadTask();
+	});
+  };
+  
+  $scope.moveItem = function(item, fromIndex, toIndex) {
+    $scope.items.splice(fromIndex, 1);
+    $scope.items.splice(toIndex, 0, item);
+  };
+  
+});
